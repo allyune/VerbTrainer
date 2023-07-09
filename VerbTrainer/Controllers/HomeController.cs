5d8218681a5cabf2a32333ce984aeddd25fd6fbe
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-07-10 00:48:16 +0200

    Displaying Verbs at the table

--- VerbTrainer/Controllers/HomeController.cs
+++ VerbTrainer/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VerbTrainer.Data;
 using VerbTrainer.Models;
+using HebrewVerbs;
 
 namespace VerbTrainer.Controllers;
 
@@ -8,20 +12,36 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, VerbTrainerDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
+
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<Verb> verbs = _dbContext.Verbs
+        .Join(_dbContext.Binyanim,
+            verb => verb.BinyanId,
+            binyan => binyan.Id,
+            (verb, binyan) => new Verb
+            {
+                Name = verb.Name,
+                Binyan = binyan.Name,
+                Root = verb.Root,
+                Meaning = verb.Meaning
+            })
+    .ToList();
+        return View(verbs);
     }
 
     public IActionResult Privacy()
     {
         return View();
     }
+
+    private readonly VerbTrainerDbContext _dbContext;
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
