5716a1b76dae51e36bf41cd5bb85d7ea71c7a29e
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-07-11 12:57:46 +0200

    Implement table filtering

---
+++ VerbTrainer/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.getElementById('verb-search');
+    searchInput.addEventListener('input', function () {
+        var searchText = this.value.toLowerCase();
+        var tableRows = document.getElementsByClassName('table-row');
+        for (var row of tableRows) {
+            var hebrewNameNikud = row.querySelector('.name').textContent.toLowerCase();
+            var hebrewName = hebrewNameNikud.replace(/([\u05B0-\u05BD]|[\u05BF-\u05C7])/g, "");
+            var englishMeaning = row.querySelector('.meaning').textContent.toLowerCase();
+            if (
+                searchText &&
+                (!hebrewName.includes(searchText) && !englishMeaning.includes(searchText))
+            ) {
+                row.style.display = 'none';
+            } else {
+                row.style.display = '';
+            }
+        }
+    });
+});
