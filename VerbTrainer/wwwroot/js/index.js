7eab6343fa065b1099a262740859a709dbd6d683
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-07-22 00:12:15 +0200

    reorganize event listeners

--- VerbTrainer/wwwroot/js/index.js
+++ VerbTrainer/wwwroot/js/index.js
@@ -1,55 +1,67 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+﻿function loadConjugations(verbId) {
+    var verbConjugations = conjugationsData.filter(verb => verb.VerbId == verbId);
+    var trainerContainer = document.getElementById('trainer-container');
+    trainerContainer.innerHTML = '';
+    for (var conjugation of verbConjugations) {
+        var conjDiv = document.createElement('div');
+        conjDiv.classList.add('conj-div')
+        //Meaning
+        var conjMeaning = document.createElement('p');
+        conjMeaning.innerHTML = conjugation.Meaning
+        conjDiv.appendChild(conjMeaning);
+        //Text
+        var conjText = document.createElement('p');
+        conjText.innerHTML = conjugation.Text
+        conjDiv.appendChild(conjText);
+        //Transcription
+        var conjTranscription = document.createElement('p');
+        conjTranscription.innerHTML = conjugation.Transcription
+        conjDiv.appendChild(conjTranscription);
+        trainerContainer.appendChild(conjDiv);
+    }
+}
+
+function tableSearch(searchText) {
+    var tableRows = document.getElementsByClassName('table-row');
+    for (var row of tableRows) {
+        var hebrewNameNikud = row.querySelector('.name').textContent.toLowerCase();
+        var hebrewName = hebrewNameNikud.replace(/([\u05B0-\u05BD]|[\u05BF-\u05C7])/g, "");
+        var englishMeaning = row.querySelector('.meaning').textContent.toLowerCase();
+        if (
+            searchText &&
+            (!hebrewName.includes(searchText) && !englishMeaning.includes(searchText))
+        ) {
+            row.style.display = 'none';
+        } else {
+            row.style.display = '';
+        }
+    }
+    };
+
+document.addEventListener('DOMContentLoaded', function () {
     var searchInput = document.getElementById('verb-search');
     searchInput.addEventListener('input', function () {
         var searchText = this.value.toLowerCase();
-        var tableRows = document.getElementsByClassName('table-row');
-        for (var row of tableRows) {
-            var hebrewNameNikud = row.querySelector('.name').textContent.toLowerCase();
-            var hebrewName = hebrewNameNikud.replace(/([\u05B0-\u05BD]|[\u05BF-\u05C7])/g, "");
-            var englishMeaning = row.querySelector('.meaning').textContent.toLowerCase();
-            if (
-                searchText &&
-                (!hebrewName.includes(searchText) && !englishMeaning.includes(searchText))
-            ) {
-                row.style.display = 'none';
-            } else {
-                row.style.display = '';
-            }
-        }
+        tableSearch(searchText)
     });
-
     var tableRows = document.querySelectorAll(".table-row");
     tableRows.forEach(row => {
         row.addEventListener('click', async function () {
             var verbId = this.getAttribute('verb-id');
             try {
-                var verbConjugations = conjugationsData.filter(verb => verb.VerbId == verbId);
-                console.log(verbConjugations);
-                var trainerContainer = document.getElementById('trainer-container');
-                trainerContainer.innerHTML = '';
-                for (var conjugation of verbConjugations) {
-                    var conjDiv = document.createElement('div');
-                    conjDiv.classList.add('conj-div')
-                    //Meaning
-                    var conjMeaning = document.createElement('p');
-                    conjMeaning.innerHTML = conjugation.Meaning
-                    conjDiv.appendChild(conjMeaning);
-                    //Text
-                    var conjText = document.createElement('p');
-                    conjText.innerHTML = conjugation.Text
-                    conjDiv.appendChild(conjText);
-                    //Transcription
-                    var conjTranscription = document.createElement('p');
-                    conjTranscription.innerHTML = conjugation.Transcription
-                    conjDiv.appendChild(conjTranscription);
-
-                    trainerContainer.appendChild(conjDiv);
-                }
-            } catch (e) {
-                console.log(e);
+                loadConjugations(verbId)
             }
-        })
+            catch (e) {
+            console.log(e);
+        }
     })
+})
 });
 
 
+
+var startTrainingButton = document.querySelector('#btn-start');
+    startTrainingButton.addEventListener(click, () => {
+        var trainerContainer = document.getElementById('trainer-container');
+        trainerContainer.innerHTML = '';
+    });
