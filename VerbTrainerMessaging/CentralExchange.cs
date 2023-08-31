d38acba7d15807e3546957f96c1a7b1e6004f56c
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-08-31 23:41:58 +0200

    Implement centrail messaging exchange

---
+++ VerbTrainerMessaging/CentralExchange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace VerbTrainerMessaging
+{
+	public static class CentralExchange
+	{
+        public static string SetupExchange(IModel channel)
+        {
+            string exchangeName = "central_exchange";
+            channel.ExchangeDeclare(
+                exchange: exchangeName,
+                type: ExchangeType.Direct
+            );
+
+            return exchangeName;
+        }
+    }
+}
+
