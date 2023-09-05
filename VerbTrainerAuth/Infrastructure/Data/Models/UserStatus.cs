c28436215d3ad7aa5924b1c9e3ac2894db993483
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-05 15:47:36 +0200

    Add UserStatus table

---
+++ VerbTrainerAuth/Infrastructure/Data/Models/UserStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VerbTrainerAuth.Infrastructure.Data.Models
+{
+	public class UserStatus : BaseAuthModel
+	{
+		public int UserStatusId { get; set; }
+		public string UserStatusName { get; set; }
+    }
+}
+
---
+++ VerbTrainerAuth/Infrastructure/Data/VerbTrainerAuthDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Design;
+using VerbTrainerAuth.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace VerbTrainerAuth.Infrastructure.Data
+{
+	public class VerbTrainerAuthDbContext : DbContext
+	{
+
+		public VerbTrainerAuthDbContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<RevokedAccessToken>()
+                .HasIndex(t => t.Token)
+                .IsUnique();
+
+            modelBuilder.Entity<RevokedRefreshToken>()
+                .HasIndex(t => t.Token)
+                .IsUnique();
+            modelBuilder.Entity<RecoveryToken>()
+                .HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.UserStatus)
+                .WithMany()
+                .HasForeignKey(u => u.StatusCode);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<RevokedAccessToken> RevokedAccessTokens { get; set; }
+        public DbSet<RevokedRefreshToken> RevokedRefreshTokens { get; set; }
+        public DbSet<RecoveryToken> RecoveryTokens { get; set; }
+        public DbSet<UserStatus> UserStatuses { get; set; }
+	}
+}
+
