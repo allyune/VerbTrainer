d09e96ce5979c3c838577ef3ba5d9bfdf2795d46
pkrasnianska <polina.krasnianska@praguecollege.cz>
2023-09-17 19:19:14 +0200

    #8: Add constructors and factory methods to Deck / Verb DTOs;

--- VerbTrainer/DTOs/DeckDto.cs
+++ VerbTrainer/DTOs/DeckDto.cs
@@ -7,8 +7,23 @@ namespace VerbTrainer.DTOs
 {
 	public class DeckDto
 	{
-        public string Name { get; set; }
-        public List<VerbDto> Verbs { get; set; }
+        public string Name { get; private set; }
+        public List<VerbDto>? Verbs { get; private set; }
+
+        private DeckDto(string name, List<VerbDto>? verbs)
+        {
+            Name = name;
+            Verbs = verbs;
+        }
+
+        public static DeckDto CreateNew(
+            string name,
+            List<VerbDto>? verbs = null)
+        {
+            return new DeckDto(name, verbs);
+        }
     }
+
+    
 }
 
